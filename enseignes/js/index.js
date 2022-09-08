f50a1ea91ff89380ee601249af6134fb1a4afa78
leo2325 <68699115+leo2325@users.noreply.github.com>
2022-09-08 17:55:26 +0200

    Add files via upload

---
+++ enseignes/js/index.js
@@ -0,0 +1,51 @@
+/* Disparition du bannerTop au clic */
+function showUnshowBannerTop(){
+	document.getElementById("bannerTop").style.display = "none";
+};
+
+
+function showUnshowDesc1(){
+	if(document.getElementById("desc1").style.display === "flex"){
+		document.getElementById("desc1").style.display = "none";} 
+	else{document.getElementById("desc1").style.display = "flex";};
+};
+function showUnshowDesc2(){
+	if(document.getElementById("desc2").style.display === "flex"){
+		document.getElementById("desc2").style.display = "none";} 
+	else{document.getElementById("desc2").style.display = "flex";};
+};
+function showUnshowDesc3(){
+	if(document.getElementById("desc3").style.display === "flex"){
+		document.getElementById("desc3").style.display = "none";} 
+	else{document.getElementById("desc3").style.display = "flex";};
+};
+function showUnshowDesc4(){
+	if(document.getElementById("desc4").style.display === "flex"){
+		document.getElementById("desc4").style.display = "none";} 
+	else{document.getElementById("desc4").style.display = "flex";};
+};
+function showUnshowDesc5(){
+	if(document.getElementById("desc5").style.display === "flex"){
+		document.getElementById("desc5").style.display = "none";} 
+	else{document.getElementById("desc5").style.display = "flex";};
+};
+function showUnshowDesc6(){
+	if(document.getElementById("desc6").style.display === "flex"){
+		document.getElementById("desc6").style.display = "none";} 
+	else{document.getElementById("desc6").style.display = "flex";};
+};
+function showUnshowDesc7(){
+	if(document.getElementById("desc7").style.display === "flex"){
+		document.getElementById("desc7").style.display = "none";} 
+	else{document.getElementById("desc7").style.display = "flex";};
+};
+function showUnshowDesc8(){
+	if(document.getElementById("desc8").style.display === "flex"){
+		document.getElementById("desc8").style.display = "none";} 
+	else{document.getElementById("desc8").style.display = "flex";};
+};
+function showUnshowDesc9(){
+	if(document.getElementById("desc9").style.display === "flex"){
+		document.getElementById("desc9").style.display = "none";} 
+	else{document.getElementById("desc9").style.display = "flex";};
+};
